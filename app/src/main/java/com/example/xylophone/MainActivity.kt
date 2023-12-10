c48ecd2532d9e01127bf84591cc62f47ac320197
Anton Glazun <127964626+Anton97G@users.noreply.github.com>
2023-12-10 23:48:03 +0600

    second commit: redefined variables via binding

--- app/src/main/java/com/example/xylophone/MainActivity.kt
+++ app/src/main/java/com/example/xylophone/MainActivity.kt
@@ -17,46 +17,38 @@ class MainActivity : AppCompatActivity() {
 
         mediaPlayer = MediaPlayer.create(this, R.raw.note_do)
 
-        val noteDo = binding.root.findViewById<Button>(R.id.note_do)
-        val noteRe = binding.root.findViewById<Button>(R.id.note_re)
-        val noteMi = binding.root.findViewById<Button>(R.id.note_mi)
-        val noteFa = binding.root.findViewById<Button>(R.id.note_fa)
-        val noteSol = binding.root.findViewById<Button>(R.id.note_sol)
-        val noteLya = binding.root.findViewById<Button>(R.id.note_lya)
-        val noteSi = binding.root.findViewById<Button>(R.id.note_si)
-        val noteDo2 = binding.root.findViewById<Button>(R.id.note_do2)
 
 
 
-        noteDo.setOnClickListener {
+        binding.noteDo.setOnClickListener {
             playSound(R.raw.note_do)
         }
 
-        noteRe.setOnClickListener {
+        binding.noteRe.setOnClickListener {
             playSound(R.raw.note_re)
         }
 
-        noteMi.setOnClickListener {
+        binding.noteMi.setOnClickListener {
             playSound(R.raw.note_mi)
         }
 
-        noteFa.setOnClickListener {
+        binding.noteFa.setOnClickListener {
             playSound(R.raw.note_fa)
         }
 
-        noteSol.setOnClickListener {
+        binding.noteSol.setOnClickListener {
             playSound(R.raw.note_sol)
         }
 
-        noteLya.setOnClickListener {
+        binding.noteLya.setOnClickListener {
             playSound(R.raw.note_lya)
         }
 
-        noteSi.setOnClickListener {
+        binding.noteSi.setOnClickListener {
             playSound(R.raw.note_si)
         }
 
-        noteDo2.setOnClickListener {
+        binding.noteDo2.setOnClickListener {
             playSound(R.raw.note_do2)
         }
 
